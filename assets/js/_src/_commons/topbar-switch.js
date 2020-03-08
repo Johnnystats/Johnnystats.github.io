7b7268ce52aa48bcecd64ef4904f40f46e15ecfd
Johnnystats <euijun.choi@yonsei.ac.kr>
2020-03-09 04:10:04 +0900

    search delete

--- assets/js/_src/_commons/topbar-switch.js
+++ assets/js/_src/_commons/topbar-switch.js
@@ -44,9 +44,6 @@ $(function() {
         $('.access').removeClass('topbar-down');
       }
 
-      if ($('#search-input').is(':focus')) {
-        $('#search-input').blur(); // remove focus
-      }
 
     } else {
       // Scroll Up
